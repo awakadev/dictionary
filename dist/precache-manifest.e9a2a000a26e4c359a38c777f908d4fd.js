62d67a42477b6b3971dc45316a021da56caa94d1
worhols <awakadev@gmail.com>
2018-09-25 22:21:20 +0300

    dist

---
+++ dist/precache-manifest.e9a2a000a26e4c359a38c777f908d4fd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e460daade20a6d99f2b",
+    "url": "/css/chunk-vendors.b63e7ae6.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6e460daade20a6d99f2b",
+    "url": "/js/chunk-vendors.a86214d1.js"
+  },
+  {
+    "revision": "19051bee9fe430cd039f",
+    "url": "/js/app.2542c065.js"
+  },
+  {
+    "revision": "73352c4eb2b7ba395977",
+    "url": "/js/add.fc8636c9.js"
+  },
+  {
+    "revision": "38691172811580aff547",
+    "url": "/js/about.34d2b80e.js"
+  },
+  {
+    "revision": "526a1a9d4d4aa31e9a2344923396931c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "19051bee9fe430cd039f",
+    "url": "/css/app.38788f5d.css"
+  },
+  {
+    "revision": "73352c4eb2b7ba395977",
+    "url": "/css/add.6f7be40f.css"
+  },
+  {
+    "revision": "38691172811580aff547",
+    "url": "/css/about.fb9fb965.css"
+  }
+];
