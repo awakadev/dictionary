3cca51271eae90207db8e450869dfadf422a532f
worhols <awakadev@gmail.com>
2018-09-28 19:40:03 +0300

    dist

---
+++ precache-manifest.e3954dbb955d23ef0dc0cc533c7593e5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f098b17f1e7ded91a55",
+    "url": "/dictionary/js/dictionary.74d5773d.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dictionary/robots.txt"
+  },
+  {
+    "revision": "08d76baeb785ce849b57",
+    "url": "/dictionary/css/games.5f9de5c4.css"
+  },
+  {
+    "revision": "a9e33dbafa9c29fbc717",
+    "url": "/dictionary/js/add.abd3219d.js"
+  },
+  {
+    "revision": "178ef8f194382250276a",
+    "url": "/dictionary/js/app.1d53e3e3.js"
+  },
+  {
+    "revision": "64416544038ef4494dfd",
+    "url": "/dictionary/js/statistics.624fb53f.js"
+  },
+  {
+    "revision": "832e8c935960dc6570e3",
+    "url": "/dictionary/js/chunk-vendors.6efbb033.js"
+  },
+  {
+    "revision": "08d76baeb785ce849b57",
+    "url": "/dictionary/js/games.e22fa64f.js"
+  },
+  {
+    "revision": "184ef974112906f7796a",
+    "url": "/dictionary/js/about.837abb4f.js"
+  },
+  {
+    "revision": "4ea71966bac2ce2f6bead4ec8dd25f97",
+    "url": "/dictionary/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/dictionary/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/dictionary/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "832e8c935960dc6570e3",
+    "url": "/dictionary/css/chunk-vendors.b63e7ae6.css"
+  },
+  {
+    "revision": "178ef8f194382250276a",
+    "url": "/dictionary/css/app.7150b669.css"
+  },
+  {
+    "revision": "a9e33dbafa9c29fbc717",
+    "url": "/dictionary/css/add.63b04e15.css"
+  }
+];
